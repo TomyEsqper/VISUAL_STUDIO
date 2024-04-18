8be7f20658edca906b9b4cf14c65caf1f889c787
TomyEsqper <tomyesqper@gmail.com>
2024-04-18 08:54:36 -0500

    Mi primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+a = 1000
+b = 1000
+
+c = a + b
+print ("El resultado de la operacion es: ", c)
+
+r = a - b
+print ("El resultado de la operacion es", r)
+
+m = a * b
+print ("El resultado de la operacion es", m)
+
+d = a / b
+print ("El resultado de la operacion es: ", d)
+
+mo = a % b
+print ("El resultado de la operacion es: ", mo)
